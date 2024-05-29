7d21baf433d8d0c1a0e07495b05288ef4f1e367e
Aryan Goel <aryangoel971@gmail.com>
2024-05-29 18:45:22 +0530

    errors

--- app/ui/dashboard/customers/page.tsx
+++ app/ui/dashboard/customers/page.tsx
@@ -1,3 +1,3 @@
-export default function () {
+export default function page() {
   return <div>Customers Page in DashBoard</div>;
 }
--- app/ui/dashboard/invoices/page.tsx
+++ app/ui/dashboard/invoices/page.tsx
@@ -1,3 +1,3 @@
-export default function () {
+export default function page() {
   return <div>Invoices Page in DashBoard</div>;
 }
